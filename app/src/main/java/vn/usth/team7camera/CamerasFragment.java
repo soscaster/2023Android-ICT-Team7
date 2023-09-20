69e6cd5979ef298b6f013f26598697735b8afa67
NTLG1 <longntl1009@gmail.com>
2023-09-20 10:21:38 +0700

    Add instruction when camera list is empty

--- app/src/main/java/vn/usth/team7camera/CamerasFragment.java
+++ app/src/main/java/vn/usth/team7camera/CamerasFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -19,6 +20,9 @@ import org.jetbrains.annotations.Nullable;
 public class CamerasFragment extends Fragment {
     private CameraListManager cameraListManager;
     private boolean isFullscreen = false;
+
+    private String addCameraText1;
+    private String addCameraText2;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +38,27 @@ public class CamerasFragment extends Fragment {
         LinearLayout camerasContainer = view.findViewById(R.id.camerasContainer);
 
         String[] cameraNames = cameraListManager.getCameraNamesArray();
+
+        addCameraText1 = getResources().getString(R.string.addCameraText1);
+        TextView addCamera1 = view.findViewById(R.id.noCamera1);
+        addCamera1.setText(addCameraText1);
+        addCameraText2 = getResources().getString(R.string.addCameraText2);
+        TextView addCamera2 = view.findViewById(R.id.noCamera2);
+        addCamera2.setText(addCameraText2);
+
+        ImageView noCameraIcon = view.findViewById(R.id.noCameraIcon);
+        noCameraIcon.setImageResource(R.drawable.baseline_add_2);
+
+        if (cameraNames.length == 0)
+        {
+            addCamera1.setVisibility(View.VISIBLE);
+            addCamera2.setVisibility(View.VISIBLE);
+            noCameraIcon.setVisibility(View.VISIBLE);}
+        else {
+            addCamera1.setVisibility(View.INVISIBLE);
+            addCamera2.setVisibility(View.INVISIBLE);
+            noCameraIcon.setVisibility(View.INVISIBLE);
+        }
 
         for (int i = 0; i < cameraNames.length; i++) {
             // Create a new instance of camera_items.xml for each camera
