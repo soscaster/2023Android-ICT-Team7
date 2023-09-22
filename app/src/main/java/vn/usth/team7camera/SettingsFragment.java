cc5ee80531e350779dff995dee40537bd078e2a5
BI12-264 Đặng Vũ Minh <dangvuminh0512@gmail.com>
2023-09-22 22:07:44 +0700

    add info for the about part

--- app/src/main/java/vn/usth/team7camera/SettingsFragment.java
+++ app/src/main/java/vn/usth/team7camera/SettingsFragment.java
@@ -285,17 +285,35 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private void showAboutDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         builder.setTitle("About App");
-        builder.setMessage("This is a description of your app.");
+
+        // Build the message with the project description and status
+        String aboutMessage = "Description:\n" +
+                "This project is a GUI-friendly application that allows users to watch and manage security camera(s). " +
+                "The application is written in (Java or Kotlin) and using some libraries for media encode/decode.\n\n" +
+                "Project Status:\n" +
+                "We started this project from Sep 11, 2023 and have not yet finished. " +
+                "The project was initially planned to be finished in 2 weeks with basic GUI and functions. " +
+                "We're in the development stage, so lots of bugs will exist.\n\n" +
+                "Project Development Details:\n" +
+                "The commit history will not show the real contribution of each member. " +
+                "We had a lot of discussions at the library to decide what to do for each project's progress. " +
+                "In order for the members not to create many structurally different pieces of code, " +
+                "we decided to focus on a single machine (leader's potato machine) during almost all development stages.";
+
+        builder.setMessage(aboutMessage);
+
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
             }
         });
+
         builder.create().show();
     }
 
-        private void setLocale(String languageCode) {
+
+    private void setLocale(String languageCode) {
         if (!getCurrentLanguage().equals(languageCode)) {
             ((MainActivity) getActivity()).saveLanguagePreference(languageCode);
             Locale newLocale = new Locale(languageCode);
