cc527ffb3bdbc0c2701eef0213a675de85b32662
Trinh Tien Minh BI12-273 <88262498+mihnnn@users.noreply.github.com>
2023-09-22 20:57:08 +0700

    Created Drawer bar
    -bug fixed: drawer bar overlapping viewpager making it not work
    -todo: add functionality for drawer bar buttons

--- app/src/main/java/vn/usth/team7camera/LoginActivity.java
+++ app/src/main/java/vn/usth/team7camera/LoginActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,5 +46,20 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            // Handle action settings click here
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/vn/usth/team7camera/MainActivity.java
+++ app/src/main/java/vn/usth/team7camera/MainActivity.java
@@ -78,27 +78,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-
-
-//        TextView username =(TextView) findViewById(R.id.username);
-//        TextView password =(TextView) findViewById(R.id.password);
-//
-//        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-//
-//        //admin and admin
-//
-//        loginbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-//                    //correct
-//                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-//                }else
-//                    //incorrect
-//                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
         requestRuntimePermission();
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
